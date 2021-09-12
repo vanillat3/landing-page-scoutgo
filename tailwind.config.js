ad3b8e5033e93876ba7d53195fa0378a4bfbe2d5
vanillat3 <84187703+vanillat3@users.noreply.github.com>
2021-09-13 00:49:41 +0700

    Add file via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '2.5rem',
+    },
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
